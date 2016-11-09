ff5006a6e13410a9a346e6f224127b7210d50fd5
Vello Vaherpuu <vello@bitweb.ee>
2016-11-09 21:28:21 +0200

    12 items per page

--- themes/sparkling/functions.php
+++ themes/sparkling/functions.php
@@ -616,3 +616,7 @@ function fig_hide_category_count()
 {
   // No count
 }
+
+
+// Display 12 products per page. Goes in functions.php
+add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 24;' ), 12 );
